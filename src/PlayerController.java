2bf1e0f773632de5a1b20ee854fcedce1ec9bc03
Isabel Ade-Hall <izzy.adehall@gmail.com>
2022-06-10 05:09:39 +0100

    Added space bar to pause, designed basic player overlay that hides when mouse stops for 3s

--- src/PlayerController.java
+++ src/PlayerController.java
@@ -1,12 +1,18 @@
-import java.io.File;
-
+import javafx.animation.PauseTransition;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class PlayerController {
     
@@ -16,6 +22,21 @@ public class PlayerController {
     @FXML
     private MediaView mediaView;
 
+    @FXML
+    private BorderPane border;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Slider slider;
+
+    @FXML
+    private Button btnPause;
+
+    @FXML
+    private Button btnFullScreen;
+
     // private int video_id;
     private Stage stage;
     private MediaPlayer mediaPlayer;
@@ -23,7 +44,25 @@ public class PlayerController {
 
     @FXML
     private void initialize() {
-        
+ 
+        BooleanProperty mouseMoving = new SimpleBooleanProperty();
+        mouseMoving.addListener((obs, wasMoving, isNowMoving) -> {
+            //Hide when mouse has stopped for set time
+            if (!isNowMoving) {
+                border.setVisible(false);
+            }
+        });
+
+        PauseTransition pause = new PauseTransition(Duration.seconds(3));
+        pause.setOnFinished(e -> mouseMoving.set(false));
+
+        stack.setOnMouseMoved((e) -> {
+            mouseMoving.set(true);
+            pause.playFromStart();
+
+            //Show when mouse moves
+            border.setVisible(true);
+        });
     }
 
     public void loadVideo(String title, Media media) {
@@ -32,12 +71,19 @@ public class PlayerController {
         stage = (Stage) stack.getScene().getWindow();
         stage.setTitle(title);
 
+        stage.getScene().setOnKeyPressed((e) -> {
+            if (e.getCode() == KeyCode.SPACE) {
+                playPause();
+            }
+        });
+
         mediaPlayer = new MediaPlayer(media);
         mediaPlayer.setAutoPlay(true);
         mediaPlayer.setOnError(()->System.out.println("media error"+mediaPlayer.getError().toString()));
         playing = true;
         mediaView.setMediaPlayer(mediaPlayer);
         mediaView.setFitWidth(800);
+        
     }
 
     @FXML
@@ -50,4 +96,5 @@ public class PlayerController {
             playing = true;
         }
     }
+
 }
