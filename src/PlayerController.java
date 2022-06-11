d6548c93536b20a47fd626b03033cf447ebbf98a
Isabel Ade-Hall <izzy.adehall@gmail.com>
2022-06-11 01:57:21 +0100

    Seek bar functionality

--- src/PlayerController.java
+++ src/PlayerController.java
@@ -2,10 +2,9 @@ import javafx.animation.PauseTransition;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.fxml.FXML;
-import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
-import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
@@ -37,10 +36,17 @@ public class PlayerController {
     @FXML
     private Button btnFullScreen;
 
+    @FXML
+    private Label lblCurrentTime;
+
+    @FXML
+    private Label lblMaxTime;
+
     // private int video_id;
     private Stage stage;
     private MediaPlayer mediaPlayer;
     private boolean playing;
+    private boolean seeking = false;
 
     @FXML
     private void initialize() {
@@ -72,18 +78,53 @@ public class PlayerController {
         stage.setTitle(title);
 
         stage.getScene().setOnKeyPressed((e) -> {
-            if (e.getCode() == KeyCode.SPACE) {
-                playPause();
+            switch (e.getCode()) {
+                case SPACE:
+                    playPause();
+                    break;
+                case LEFT:
+                    backTen();
+                    break;
+                case RIGHT:
+                    forwardTen();
+                    break;
+                default:
+                    break;
             }
         });
 
         mediaPlayer = new MediaPlayer(media);
-        mediaPlayer.setAutoPlay(true);
+        mediaPlayer.setOnReady(() -> {
+            //Once mediaPlayer has loaded media
+            slider.setMax(media.getDuration().toMinutes());
+            lblMaxTime.setText(minutesToHMS(slider.getMax()));
+            mediaPlayer.play();
+            playing = true;
+        });
         mediaPlayer.setOnError(()->System.out.println("media error"+mediaPlayer.getError().toString()));
-        playing = true;
         mediaView.setMediaPlayer(mediaPlayer);
         mediaView.setFitWidth(800);
+
+        //When mediaPlayer time changes, update slider value and current time label
+        mediaPlayer.currentTimeProperty().addListener((obs, oldTime, newTime) -> {
+            if (!seeking) {
+                slider.setValue(newTime.toMinutes());
+            }
+            lblCurrentTime.setText(minutesToHMS(newTime.toMinutes()));
+        });
+
+        //If slider value changes to something different than current mediaPlayer time, update mediaPlayer time
+        slider.setOnMouseReleased((e) -> {
+            if (slider.getValue() != mediaPlayer.getCurrentTime().toMinutes()){
+                mediaPlayer.seek(Duration.minutes(slider.getValue()));
+            }
+            seeking = false;
+        });
         
+        //While dragging the automatic update of slider with time stops (so it doesn't jerk around)
+        slider.setOnDragDetected((e) -> {
+            seeking = true;
+        });
     }
 
     @FXML
@@ -95,6 +136,39 @@ public class PlayerController {
             mediaPlayer.play();
             playing = true;
         }
+    }
+
+    // @FXML
+    private void backTen() {
+        mediaPlayer.seek(mediaPlayer.getCurrentTime().subtract(Duration.seconds(10)));
+    }
+
+    // @FXML
+    private void forwardTen() {
+        mediaPlayer.seek(mediaPlayer.getCurrentTime().add(Duration.seconds(10)));
+    }
+
+    private String minutesToHMS(double mins) {
+
+        double tempMins = mins;
+        int hours = 0;
+        int minutes = 0;
+        int seconds = 0;
+
+        if (mins >= 60) {
+            hours = (int)(tempMins / 60);
+            tempMins -= hours * 60;
+        }
+
+        minutes = (int)tempMins;
+        seconds = (int)((tempMins - minutes) * 60);
+
+        String HMS = String.format("%02d:%02d", minutes, seconds);
+        if (hours != 0) {
+            HMS = String.format("%02d:"+HMS, hours);
+        }
+
+        return HMS;
     }
 
 }
